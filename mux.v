19eaaddce6a97ad410d54f4798de875753bc7132
pouyarahmti <39616083+pouyarahmti@users.noreply.github.com>
2018-12-16 16:40:54 +0330

    mux has delay

--- mux.v
+++ mux.v
@@ -6,15 +6,7 @@ module mux( out  , address , in1 , in2 ,);
 	input address;
 
 	//using reg for changing the data in the always
-	output reg [n-1:0] out;
+	output [n-1:0] out;
 
-	always begin 
-		#delay
-		if(address == 0) begin
-			out <= in1;
-		end
-		else if (address == 1) begin 
-			out <= in2;
-		end 
-	end
+	assign #delay out = (address == 0 ) ? in1 : in2; 
 endmodule 
