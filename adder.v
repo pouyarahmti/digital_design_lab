dca795960cadb4bf4eb13c3eab7fd8746444eeb4
pouyarahmti <39616083+pouyarahmti@users.noreply.github.com>
2018-12-16 16:44:05 +0330

    adder has delay

--- adder.v
+++ adder.v
@@ -1,7 +1,8 @@
 module adder(in1,in2,  out);
+	parameter delay = 5;
 	input [63:0] in2;
 	input [63:0] in1;
 	output[63:0] out;  
 
-	assign out = in1 + in2;
+	assign #delay out = in1 + in2;
 endmodule
