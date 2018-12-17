a37344d7649d1baa3deb5bd783ea33c95205ca4f
pouyarahmti <39616083+pouyarahmti@users.noreply.github.com>
2018-12-17 18:48:57 +0330

    sign extend added

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -1,7 +1,15 @@
-module signextend(in, out);
+module signextend(instruction , extended_address );
 
-	//should be changed for including evey type of instruction immediate
-	input [31:0] in;
-	output [63:0] out;
-	assign out = {{32{in[31]}}, in};
+	input [31:0] instruction ;
+	output reg [63:0] extended_address;
+	
+	wire opcode = instruction[31:30]; 
+	
+	always @(*) begin
+		case(opcode) 
+			2'b00: extended_address = {{38{instruction[25]}} ,instruction[25:0] };
+			2'b10: extended_address = {{45{instruction[23]}} ,instruction[23:5] };
+			2'b11: extended_address = {{55{instruction[20]}} ,instruction[20:12] };
+		endcase
+	end	
 endmodule
